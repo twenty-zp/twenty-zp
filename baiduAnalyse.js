0c1bfe0a5fa45ac4f0559bc576a3f1de0d31b489
zp <573561611@qq.com>
2016-10-17 10:43:37 +0800

    添加百度统计

---
+++ baiduAnalyse.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?a19dd0c033f48c51a16dc7871acded18";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
