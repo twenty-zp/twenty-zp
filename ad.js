de587853f3a9c081af8f7ff9dedf214ad36f3220
zp <573561611@qq.com>
2017-06-19 15:31:12 +0800

    iOS使用Jenkins构建Cordova

--- ad.js
+++
@@ -1,9 +0,0 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- 广告1 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-5863089843360958"
-     data-ad-slot="7907461226"
-     data-ad-format="auto"></ins>
-
-(adsbygoogle = window.adsbygoogle || []).push({});
